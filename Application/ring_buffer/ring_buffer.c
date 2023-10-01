a7f8190ba7650b6d4ad0a8e3fc80ec91d733bdd8
russelmallari <mallari.russelroman@gmail.com>
2023-10-01 16:26:46 +0800

    Added features to ring buffer

--- Application/ring_buffer/ring_buffer.c
+++ Application/ring_buffer/ring_buffer.c
@@ -1,9 +1,15 @@
+#include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
 
 #include "stm32f1xx_hal.h"
 
 #include "ring_buffer.h"
+
+
+static void increment_count(ring_buf_t * p_ring_buf);
+static void decrement_count(ring_buf_t * p_ring_buf);
+static void wrap_around_index_check(ring_buf_t * p_ring_buf);
 
 
 void ring_buf_init(ring_buf_t * p_ring_buf)
@@ -15,65 +21,127 @@ void ring_buf_init(ring_buf_t * p_ring_buf)
 	memset(p_ring_buf->buf, 0U, sizeof(p_ring_buf->buf));
 }
 
+
 void ring_buf_put(ring_buf_t * p_ring_buf, uint8_t data)
 {
 	// Overwrite the existing data with new data
-	// when buffer is full
-	uint32_t masking_tate;
+	// when buffer is full.
 
-	if(p_ring_buf->count == BUF_SIZE)
+	if(p_ring_buf->count < BUF_SIZE)
 	{
-		p_ring_buf->count = 0U;
+		increment_count(p_ring_buf);
 	}
 	else
 	{
-		masking_tate = __get_PRIMASK();
-		__disable_irq();
-
-		++(p_ring_buf->count);
-
-		__set_PRIMASK(masking_tate);
+		p_ring_buf->count = 0U;
 	}
 
 	p_ring_buf->buf[p_ring_buf->head] = data;
 	++(p_ring_buf->head);
 
-	if(p_ring_buf->head > (BUF_SIZE - 1U))
-	{
-		p_ring_buf->head = 0U;
-	}
-
-
-
+	wrap_around_index_check(p_ring_buf);
 }
+
 
 int ring_buf_get(ring_buf_t * p_ring_buf)
 {
 	uint8_t data;
-	uint32_t masking_tate;
 
-	if(p_ring_buf->count == 0U)
-	{
-
-	}
-	else
+	if(p_ring_buf->count > 0U)
 	{
 		data = p_ring_buf->buf[p_ring_buf->tail];
 		++(p_ring_buf->tail);
 
-		masking_tate = __get_PRIMASK();
-		__disable_irq();
+		decrement_count(p_ring_buf);
+		wrap_around_index_check(p_ring_buf);
+	}
+	else
+	{
 
-		--(p_ring_buf->count);
-
-		__set_PRIMASK(masking_tate);
-
-
-		if(p_ring_buf->tail > (BUF_SIZE - 1U))
-		{
-			p_ring_buf->tail = 0U;
-		}
 	}
 
 	return data;
 }
+
+
+bool ring_buf_is_empty(ring_buf_t * p_ring_buf)
+{
+	bool is_empty = false;
+
+	if(p_ring_buf->count == 0)
+	{
+		is_empty = true;
+	}
+	else
+	{
+		is_empty = false;
+	}
+
+	return is_empty;
+}
+
+
+bool ring_buf_is_full(ring_buf_t * p_ring_buf)
+{
+	bool is_full = false;
+
+	if(p_ring_buf->count == BUF_SIZE)
+	{
+		is_full = true;
+	}
+	else
+	{
+		is_full = false;
+	}
+
+	return is_full;
+}
+
+bool ring_buf_num_contents(ring_buf_t * p_ring_buf)
+{
+	return p_ring_buf->count;
+}
+
+static void wrap_around_index_check(ring_buf_t * p_ring_buf)
+{
+	if(p_ring_buf->tail > (BUF_SIZE - 1U))
+	{
+		p_ring_buf->tail = 0U;
+	}
+}
+
+
+static void increment_count(ring_buf_t * p_ring_buf)
+{
+	uint32_t masking_tate;
+	masking_tate = __get_PRIMASK();
+	__disable_irq();
+
+	++(p_ring_buf->count);
+
+	__set_PRIMASK(masking_tate);
+}
+
+
+static void decrement_count(ring_buf_t * p_ring_buf)
+{
+	uint32_t masking_tate;
+	masking_tate = __get_PRIMASK();
+	__disable_irq();
+
+	--(p_ring_buf->count);
+
+	__set_PRIMASK(masking_tate);
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- Application/ring_buffer/ring_buffer.h
+++ Application/ring_buffer/ring_buffer.h
@@ -16,5 +16,8 @@ typedef struct ring_buf
 void ring_buf_init(ring_buf_t * p_ring_buf);
 void ring_buf_put(ring_buf_t * p_ring_buf, uint8_t data);
 int ring_buf_get(ring_buf_t * p_ring_buf);
+bool ring_buf_is_empty(ring_buf_t * p_ring_buf);
+bool ring_buf_is_full(ring_buf_t * p_ring_buf);
+bool ring_buf_num_contents(ring_buf_t * p_ring_buf);
 
 #endif /* RING_BUFFER_H */
